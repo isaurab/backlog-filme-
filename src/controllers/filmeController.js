f220f528a992d20fbe26194e5c13d35c348f9cd4
ifnmg <ifnmg@localhost.localdomain>
2024-08-14 22:21:54 -0300

    add método de post

--- src/controllers/filmeController.js
+++ src/controllers/filmeController.js
@@ -1,6 +1,7 @@
 import filmeModel from "../models/filmeModel.js";
 
 export default class Filmes {
+
     async BuscarTodosOsFilmes (req, res){
         try {
             const filmes = await filmeModel.findAll();
@@ -8,10 +9,20 @@ export default class Filmes {
         }
         catch (err){
             res.status(500).json({erro: err.message});
-
         }
+        
     }
 
+    async CadastrarFilme (req, res) {
+        try {
+            const filmeCadastrado = await filmeModel.create(req.body);
+            res.json({
+                message:"Filme criado com sucesso!",
+                filmeCadastrado
+            });
 
-
+        } catch (error) {
+            res.status(500).json({erro: error.message});
+        }
+    }
 }
--- src/routes/filmeRoutes.js
+++ src/routes/filmeRoutes.js
@@ -1,4 +1,4 @@
-import Express from "express";
+import express from "express";
 import filmeController from "../controllers/filmeController.js";
  
 const router = express.Router();
@@ -8,9 +8,18 @@ router.get('/', (req, res) => {
     try {
         filme.BuscarTodosOsFilmes();
     } catch(err){
+        
         res.status(500).json({erro: err.message}) //
     }
  });
+
+ router.post('/', (req, res) => {
+    try {
+        filme.CadastrarFilme(req, res);
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+ })
 
  export default router;
 
